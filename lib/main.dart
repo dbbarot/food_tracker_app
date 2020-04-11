1667e7f971f05dc5e5d87ee057cbd35031e40405
Dhairya <dhairyabarot10@gmail.com>
2020-04-10 19:48:31 -0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'login_signup_page.dart';
+import 'package:food_tracker_app/authentication.dart';
+import 'package:food_tracker_app/root_page.dart';
+
+void main() {
+  runApp(new MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+        title: 'Food Tracker App',
+        debugShowCheckedModeBanner: false,
+        theme: new ThemeData(
+          primarySwatch: Colors.green,
+        ),
+        home: new RootPage(auth: new Auth()));
+  }
+}
